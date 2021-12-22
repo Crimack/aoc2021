3608b27c80b526c442573b8a30861ed713b5ad24
Chris McKee <chrismckee@monzo.com>
2021-12-22 14:35:40 +0000

    20 working

--- src/day20/day20.kt
+++ src/day20/day20.kt
@@ -3,41 +3,48 @@ package day20
 import readInput
 
 fun main() {
-    val lines = readInput("day20/Day20Test")
+    val lines = readInput("day20/Day20")
+//    val lines = readInput("day20/Day20Test")
 
     val legend = lines[0]
     println(legend.length)
 
     var curr =
         lines.subList(2, lines.size).flatMapIndexed { i, row -> row.mapIndexed { j, char -> Pair(Pair(i, j), char) } }
-            .filter { it.second != '.' }
             .toMap()
 
     printLit(curr)
 
+    val xValues = curr.keys.map { it.first }.sorted()
+    var minX = xValues.first()
+    var maxX = xValues.last()
 
-    for (step in 1 until 3) {
-        println("Step $step")
+    val yValues = curr.keys.map { it.second }.sorted()
+    var minY = yValues.first()
+    var maxY = yValues.last()
 
-        val xValues = curr.keys.map { it.first }.sorted()
-        val minX = xValues.first() - 1
-        val maxX = xValues.last() + 2
-
-        val yValues = curr.keys.map { it.second }.sorted()
-        val minY = yValues.first() - 1
-        val maxY = yValues.last() + 2
-
-        println("Drawing box $minX..$maxX, $minY..$maxY")
+    for (step in 1..50) {
+        println("Step $step, box $minX..$maxX, $minY..$maxY")
 
         val next = mutableMapOf<Pair<Int, Int>, Char>()
-        for (x in minX until maxX) {
-            for (y in minY until maxY) {
-                print(curr.getOrDefault(Pair(x, y), '.'))
+        for (x in minX - 1..maxX + 1) {
+            for (y in minY - 1..maxY + 1) {
 
                 val tile = mutableListOf<Char>()
-                for (i in x - 1 until x + 2) {
-                    for (j in y - 1 until y + 2) {
-                        tile.add(curr.getOrDefault(Pair(i, j), '.'))
+                for (i in x - 1..x + 1) {
+                    for (j in y - 1..y + 1) {
+                        tile.add(
+                            curr.getOrDefault(
+                                Pair(i, j), when (legend.first() == '#') {
+                                    // The space around the known area of the board will always be binary 000000.
+                                    // This effectively means that it'll 'blink' from . -> # repeatedly, and we need
+                                    // to treat the unknown space differently depending on how many iterations we've
+                                    // been through.
+                                    true -> if (step % 2 == 0) legend.first() else legend.last()
+                                    false -> '.'
+                                }
+                            )
+                        )
                     }
                 }
                 if (tile.size != 9) {
@@ -52,18 +59,20 @@ fun main() {
                     }
                 }.joinToString("").toInt(2)
 
-                if(legend[index] != '.') {
-                    next[Pair(x, y)] = legend[index]
-                }
+                next[Pair(x, y)] = legend[index]
             }
-            println()
         }
         curr = next
-        printLit(curr)
+        minX--
+        minY--
+        maxX++
+        maxY++
     }
+    printLit(curr)
 }
 
 fun printLit(curr: Map<Pair<Int, Int>, Char>) {
+    println("Size: ${curr.size}")
     println("Lit: " + curr.values.map {
         when (it) {
             '.' -> 0
